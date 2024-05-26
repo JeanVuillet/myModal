f002efb9fc599446126f039267fcfe8d503b9dee
Jean Vuillet <vuillet433@hotmail.com>
2024-05-26 09:52:55 -0500

    working version?

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,13 @@ import React, { useRef } from "react";
 import Modal from "./lib/index.jsx";
 
 function App() {
-  const buttonOpen = useRef(null);
-  const modalElement = useRef(null);
 
-  function addPerson(e) {
-    e.preventDefault();
-    console.log("Add person !!", e.target.name.value);
-    modalElement.current.style.display = "none";
-  }
 
   return (
     <>
       <div>
-        Pour ouvrir le formulaire
-        <button type="button" ref={buttonOpen}>
-          {" "}
-          cliquez ici
-        </button>
-      </div>
-      <div id="personModal" ref={modalElement}>
-        <Modal
-          addPerson={addPerson}
-          buttonOpen={buttonOpen}
-          modalElement={modalElement}
-        />
+    coucou monde
+        <Modal/>
       </div>
     </>
   );
