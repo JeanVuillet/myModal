09488a74041ee8c4af18fc429d1d92d73fcbd620
Jean Vuillet <vuillet433@hotmail.com>
2024-05-26 17:26:53 -0500

    ok

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,65 +1,62 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
+const ModalContainer = styled.div`
+  position: absolute;
+  width: 30%;
+  height: 30%;
+  color: black;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  left: 50%;
+  background-color: lightgrey;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  border: solid black;
+`;
 
+const Modal = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
- function MyModal( { open, containerObject, messageObject, buttonObject, modalMessage, buttonMessage}){
+const ModalButton = styled.button`
+  display: flex;
+  justify-content: center;
+  width: 30%;
+  height: auto;
+`;
 
-     const [isOpen, setIsOpen]=useState(true);
+function MyModal({ open, modalMessage, buttonMessage }) {
+  const [isOpen, setIsOpen] = useState(true);
 
-     useEffect(()=>{setIsOpen(!isOpen)},[open])
-    const ModalContainer=styled.div`
-    position: absolute;
-    width: 30%;
-    height: 30%;
-    color: black;
-    top:50%;
-    transform: translate(-50%,-50%);
-    left: 50%;
-    background-color:lightgrey;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    border:solid black;
+  useEffect(() => {
+    setIsOpen(open);
+  }, [open]);
 
-    ${containerObject}
-    `;
-   const Modal=styled.div`
-   display:flex;
-   justify-content:center;
-   ${messageObject}
-  
-   `
-    const ModalButton=styled.button`
-    display:flex;
-    justify-content:center;
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-    width:30%;
-    hight:auto;
-     ${buttonObject}
-    `
-  
-
-
-    function closeModal(){
-        setIsOpen(false)
-
-    }
-    
-    return(
-     
-        <ModalContainer className="modalContainer"  style={isOpen?{display:'flex'}:{display:'none'}}>
-          
-            <Modal className="modalMessage">
-                {modalMessage? modalMessage:'operation validée'}
-
-            </Modal>
-            <ModalButton onClick={closeModal} className='modalButton'> {buttonMessage?buttonMessage:'ok'}</ModalButton>
-        </ModalContainer>
-
-    )
+  return (
+    <ModalContainer className="modalContainer" style={{ display: isOpen ? 'flex' : 'none' }}>
+      <Modal className="modalMessage">
+        {modalMessage ? modalMessage : 'Opération validée'}
+      </Modal>
+      <ModalButton onClick={closeModal} className="modalButton">
+        {buttonMessage ? buttonMessage : 'OK'}
+      </ModalButton>
+    </ModalContainer>
+  );
 }
-export default MyModal
+
+MyModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  modalMessage: PropTypes.string,
+  buttonMessage: PropTypes.string,
+};
+
+export default MyModal;
