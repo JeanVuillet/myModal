c76c634249f06ca99c01cfcd260c90ad9902fac8
Jean Vuillet <vuillet433@hotmail.com>
2024-05-26 16:56:44 -0500

    ok

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,16 +1,65 @@
-/* eslint-disable react/prop-types */
-import React, { useEffect } from "react";
-import "./style.css";
-
-export default function Modal(props) {
-
+import React from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useEffect } from 'react';
 
 
 
+ function MyModal( { open, containerObject, messageObject, buttonObject, modalMessage, buttonMessage}){
 
-  return (
-    <div>
-hello world
-    </div>
-  );
+     const [isOpen, setIsOpen]=useState(true);
+
+     useEffect(()=>{setIsOpen(!isOpen)},[open])
+    const ModalContainer=styled.div`
+    position: absolute;
+    width: 30%;
+    height: 30%;
+    color: black;
+    top:50%;
+    transform: translate(-50%,-50%);
+    left: 50%;
+    background-color:lightgrey;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border:solid black;
+
+    ${containerObject}
+    `;
+   const Modal=styled.div`
+   display:flex;
+   justify-content:center;
+   ${messageObject}
+  
+   `
+    const ModalButton=styled.button`
+    display:flex;
+    justify-content:center;
+
+    width:30%;
+    hight:auto;
+     ${buttonObject}
+    `
+  
+
+
+    function closeModal(){
+        setIsOpen(false)
+
+    }
+    
+    return(
+     
+        <ModalContainer className="modalContainer"  style={isOpen?{display:'flex'}:{display:'none'}}>
+          
+            <Modal className="modalMessage">
+                {modalMessage? modalMessage:'operation validée'}
+
+            </Modal>
+            <ModalButton onClick={closeModal} className='modalButton'> {buttonMessage?buttonMessage:'ok'}</ModalButton>
+        </ModalContainer>
+
+    )
 }
+export default MyModal
